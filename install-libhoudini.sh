a8c54bc679e3d5cd88a028e281c41b53eeea1560
Jon West <electrikjesus@gmail.com>
2022-01-08 18:30:34 -0500

    Add shell scripts for each option
    This will allow it to work with the Waydroid-Settings GTK app

---
+++ install-libhoudini.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+this_file="install-libhoudini"
+
+dir=$(pwd)
+echo $dir
+file=$(find $dir -name $this_file.sh)
+dirname="${file%/*}"
+cd $dirname
+python waydroid_extras.py --$this_file
