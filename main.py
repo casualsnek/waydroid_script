6922bdba0deaa5b95c29035bf23d878286d2a84f
Rikka <ayasa0520@gmail.com>
2023-04-29 19:10:24 +0800

    microg: use MinMicroG

--- main.py
+++ main.py
@@ -13,6 +13,82 @@ from stuffs.nodataperm import Nodataperm
 from stuffs.smartdock import Smartdock
 from stuffs.widevine import Widevine
 import tools.helper as helper
+
+
+import argparse
+
+# def main():
+#     def get_certified():
+#         print("Calling android_get_certified function")
+
+#     def install_app(app_name):
+#         print(f"Installing {app_name}")
+
+#     def remove_app(app_name):
+#         print(f"Removing {app_name}")
+
+#     def hack_option(option_name):
+#         print(f"Enabling {option_name}")
+
+#     parser = argparse.ArgumentParser(description='''
+#     Does stuff like installing Gapps, installing Magisk, installing NDK Translation and getting Android ID for device registration.
+#     Use -h  flag for help!''')
+
+#     subparsers = parser.add_subparsers(dest='command')
+
+#     # android command
+#     certified = subparsers.add_parser('certified', help='Get device ID to obtain Play Store certification')
+#     certified.set_defaults(func=get_certified)
+
+#     install_choices=["gapps", "microg", "libndk", "libhoudini", "magisk", "smartdock", "widevine"],
+#     hack_choices = ["nodataperm", "hidestatusbar"]
+#     remove_choices=install_choices
+
+#     arg_template = {
+#         "dest": "app",
+#         "type": str,
+#         "nargs": '+',
+#         "metavar":"",
+#     }
+
+#     install_help = """
+# gapps: Install Open GApps (Android 11) or MindTheGapps (Android 13)
+# microg: Add microG, Aurora Store and Aurora Droid to WayDriod
+# libndk: Add libndk arm translation, better for AMD CPUs
+# libhoudini: Add libhoudini arm translation, better for Intel CPUs
+# magisk: Install Magisk Delta to WayDroid
+# smartdock: A desktop mode launcher for Android
+# widevine: Add support for widevine DRM L3
+#     """
+#     # install and its aliases
+#     install_parser = subparsers.add_parser('install', aliases=['-i'], formatter_class=argparse.RawTextHelpFormatter, help='Install an app')
+#     install_parser.add_argument(**arg_template, help=install_help)
+#     install_parser.set_defaults(func=install_app)
+
+#     # remove and its aliases
+#     remove_parser = subparsers.add_parser('remove', aliases=['-r'], help='Remove an app')
+#     remove_parser.add_argument(**arg_template, help='Name of app to remove')
+#     remove_parser.set_defaults(func=remove_app)
+
+#     # hack and its aliases
+#     hack_parser = subparsers.add_parser('hack', aliases=['-h'], help='Hack the system')
+#     hack_parser.add_argument('option_name', choices=["nodataperm", "hidestatus"], help='Name of hack option')
+#     hack_parser.set_defaults(func=hack_option)
+
+#     args = parser.parse_args()
+#     if hasattr(args, 'func'):
+#         args_dict = vars(args)
+#         if 'app_name' in args_dict:
+#             args.func(args_dict['app_name'])
+#         else:
+#             args.func(args_dict['option_name'])
+#     else:
+#         parser.print_help()
+
+
+
+
+
 def install(args):
     app = args.app
     if "gapps" in app:
@@ -90,17 +166,18 @@ def main():
         "type": str,
         "nargs": '+',
         "metavar":"",
-        "choices": ["gapps", "microg", "libndk", "libhoudini", "magisk", "smartdock", "widevine", "nodataperm", "hidestatus"],
     }
 
     install_help = """
-gapps: Install Open GApps Pico(minimum GApps installation)
+gapps: Install Open GApps (Android 11) or MindTheGapps (Android 13)
 microg: Add microG, Aurora Store and Aurora Droid to WayDriod
 libndk: Add libndk arm translation, better for AMD CPUs
 libhoudini: Add libhoudini arm translation, better for Intel CPUs
 magisk: Install Magisk Delta to WayDroid
 smartdock: A desktop mode launcher for Android
+widevine: Add support for widevine DRM L3
     """
+
     install_parser = subparsers.add_parser("install",formatter_class=argparse.RawTextHelpFormatter, help='install something')
     install_parser.set_defaults(func=install)
     install_parser.add_argument(**arg_template,help=install_help)
