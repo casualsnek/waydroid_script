a3f6cf0912c2660ef228e0675b1a47b734f9a6fe
burgerzuhsaga <130773996+burgerzuhsaga@users.noreply.github.com>
2023-07-30 22:24:39 +0300

    Update images.py

--- tools/images.py
+++ tools/images.py
@@ -24,8 +24,8 @@ def umount(mount_point, exists=True):
             mount_point))
 
 def resize(img_file, size):
-    run(["sudo", "e2fsck", "-y", "-f", img_file], ignore="^e2fsck \d+\.\d+\.\d (.+)\n$")
-    run(["sudo", "resize2fs", img_file, size], ignore="^resize2fs \d+\.\d+\.\d (.+)\n$")
+    run(["e2fsck", "-y", "-f", img_file], ignore="^e2fsck \d+\.\d+\.\d (.+)\n$")
+    run(["resize2fs", img_file, size], ignore="^resize2fs \d+\.\d+\.\d (.+)\n$")
 
 def get_image_dir():
     # Read waydroid config to get image location
